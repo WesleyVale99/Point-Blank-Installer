87bfcf29c1d0d1e5fd476c5c38fef91b4c9ffa63
WesleyVale99 <78268026+WesleyVale99@users.noreply.github.com>
2021-01-30 12:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ installer/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_install = new System.Windows.Forms.Button();
+            this.btn_unistall = new System.Windows.Forms.Button();
+            this.btn_Repair = new System.Windows.Forms.Button();
+            this.lbl_Aguardando = new System.Windows.Forms.Label();
+            this.lbl_transfer = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 161);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(438, 10);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btn_install
+            // 
+            this.btn_install.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_install.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_install.Location = new System.Drawing.Point(319, 79);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(142, 40);
+            this.btn_install.TabIndex = 1;
+            this.btn_install.Text = "Instalar";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // btn_unistall
+            // 
+            this.btn_unistall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_unistall.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_unistall.Location = new System.Drawing.Point(171, 79);
+            this.btn_unistall.Name = "btn_unistall";
+            this.btn_unistall.Size = new System.Drawing.Size(142, 40);
+            this.btn_unistall.TabIndex = 2;
+            this.btn_unistall.Text = "Desinstalar";
+            this.btn_unistall.UseVisualStyleBackColor = true;
+            this.btn_unistall.Click += new System.EventHandler(this.btn_unistall_Click);
+            // 
+            // btn_Repair
+            // 
+            this.btn_Repair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Repair.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Repair.Location = new System.Drawing.Point(23, 79);
+            this.btn_Repair.Name = "btn_Repair";
+            this.btn_Repair.Size = new System.Drawing.Size(142, 40);
+            this.btn_Repair.TabIndex = 3;
+            this.btn_Repair.Text = "Reparar";
+            this.btn_Repair.UseVisualStyleBackColor = true;
+            this.btn_Repair.Click += new System.EventHandler(this.btn_Repair_Click);
+            // 
+            // lbl_Aguardando
+            // 
+            this.lbl_Aguardando.AutoSize = true;
+            this.lbl_Aguardando.ForeColor = System.Drawing.Color.White;
+            this.lbl_Aguardando.Location = new System.Drawing.Point(19, 178);
+            this.lbl_Aguardando.Name = "lbl_Aguardando";
+            this.lbl_Aguardando.Size = new System.Drawing.Size(123, 13);
+            this.lbl_Aguardando.TabIndex = 4;
+            this.lbl_Aguardando.Text = "Aguardando o download";
+            // 
+            // lbl_transfer
+            // 
+            this.lbl_transfer.AutoSize = true;
+            this.lbl_transfer.ForeColor = System.Drawing.Color.White;
+            this.lbl_transfer.Location = new System.Drawing.Point(430, 145);
+            this.lbl_transfer.Name = "lbl_transfer";
+            this.lbl_transfer.Size = new System.Drawing.Size(27, 13);
+            this.lbl_transfer.TabIndex = 5;
+            this.lbl_transfer.Text = " 0 %";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::installer.Properties.Resources.img_36304_20170701192343;
+            this.pictureBox1.Location = new System.Drawing.Point(225, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 292);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(225, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 21);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 134);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_transfer);
+            this.Controls.Add(this.lbl_Aguardando);
+            this.Controls.Add(this.btn_Repair);
+            this.Controls.Add(this.btn_unistall);
+            this.Controls.Add(this.btn_install);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Point Blank Installer";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_install;
+        private System.Windows.Forms.Button btn_unistall;
+        private System.Windows.Forms.Button btn_Repair;
+        private System.Windows.Forms.Label lbl_Aguardando;
+        private System.Windows.Forms.Label lbl_transfer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ installer/Form1.cs
@@ -0,0 +1,215 @@
+﻿using Ionic.Zip;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace installer
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public WebClient Client = new WebClient();
+        public string path = Application.StartupPath + "\\PointBlank.zip";
+        public string PathAdd;
+        public bool stts;
+        public Form1()
+        {
+            InitializeComponent();
+            Client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(install_DownloadProgressChanged);
+            Client.DownloadFileCompleted += new AsyncCompletedEventHandler(Install_zip_DownloadProgressCompleted);
+        }
+
+        private void Install_zip_DownloadProgressCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            PathAdd = PathInstall();
+            if (PathAdd != string.Empty)
+                unzip(PathAdd, path);
+            else
+                MessageBox.Show("o caminho especificado não pode ser vazio.", "Menu - Download", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        private void install_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            lbl_Aguardando.Text = "Aguarde o jogo está sendo baixado...";
+            BarTransfer(e.BytesReceived, e.TotalBytesToReceive, e.ProgressPercentage);
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            #region Animation
+            new Thread(() =>
+            {
+                try
+                {
+                    Thread.Sleep(2);
+                    ClientSize = new Size(493, 212); //134
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }).Start();
+            #endregion
+            stts = false;
+            Client.DownloadFileAsync(new Uri("http://127.0.0.1/arquivos/clientes/PointBlank.zip"), path);
+            Application.DoEvents();
+        }
+        public string PathInstall()
+        {
+            FolderBrowserDialog fbd1 = new FolderBrowserDialog
+            {
+                Description = "Selecione um diretório especifico: ",
+                RootFolder = Environment.SpecialFolder.MyComputer,
+                ShowNewFolderButton = true
+            };
+            if (fbd1.ShowDialog() == DialogResult.OK)
+                return fbd1.SelectedPath;
+            return "";
+        }
+        private void btn_unistall_Click(object sender, EventArgs e)
+        {
+            #region Animation
+            new Thread(() =>
+            {
+                try
+                {
+                    Thread.Sleep(2);
+                    ClientSize = new Size(493, 212); //134
+                    lbl_Aguardando.Text = "Tentando desinstalar arquivos.";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }).Start();
+            #endregion
+            StreamReader streamReader = null;
+            try
+            {
+                 streamReader = new StreamReader(@"C:\Temp\directory.txt");
+                if (streamReader != null)
+                {
+                    string path = streamReader.ReadLine();
+                    Directory.Delete(path);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if(streamReader != null)
+                {
+                    streamReader.Dispose();
+                    streamReader.Close();
+                }
+            }
+
+            Application.DoEvents();
+        }
+
+        private void btn_Repair_Click(object sender, EventArgs e)
+        {
+            #region Animation
+            new Thread(() =>
+            {
+                try
+                {
+                    Thread.Sleep(2);
+                    ClientSize = new Size(493, 212); //134
+                    lbl_Aguardando.Text = "Tentando reparar arquivos.";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }).Start();
+            #endregion
+            Application.DoEvents();
+        }
+        public void BarTransfer(long received, long maximum, long Porcentagem)
+        {
+            lbl_transfer.Text = string.Concat(Porcentagem + " %");
+            progressBar1.Width = (int)(received * 438 / maximum);
+        }
+        public void unzip(string TargetDir, string ZipToUnpack)
+        {
+            try
+            {
+                stts = true;
+                ZipFile zipFile = ZipFile.Read(ZipToUnpack);
+                try
+                {
+                    zipFile.ExtractProgress += new EventHandler<ExtractProgressEventArgs>(unzip_ExtractProgressChanged);
+                    foreach (ZipEntry zipEntry1 in zipFile)
+                    {
+                        string fileName = zipEntry1.FileName;
+                        if (fileName.Contains("/"))
+                        {
+                            int num2 = fileName.LastIndexOf("/");
+                            fileName = fileName.Substring(num2 + 1);
+                        }
+                        zipEntry1.Extract(TargetDir, ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+                finally
+                {
+                    if (zipFile != null)
+                    {
+                        zipFile.Dispose();
+                    }
+                    lbl_Aguardando.Text = "Instalação concluida!";
+                    InstallerTemp();
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+        public void InstallerTemp()
+        {
+            StreamWriter streamwrite = null;
+            try
+            {
+                streamwrite = new StreamWriter(new FileStream(@"C:\Temp\directory.txt", FileMode.Create));
+                streamwrite.WriteLine(PathAdd);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if(streamwrite != null)
+                {
+                    streamwrite.Dispose();
+                    streamwrite.Close();
+                }
+            }
+        }
+        private void unzip_ExtractProgressChanged(object sender, ExtractProgressEventArgs e)
+        {
+            Application.DoEvents();
+            if (e.TotalBytesToTransfer > 0)
+            {
+                lbl_Aguardando.Text = e.ArchiveName;
+                lbl_transfer.Text = string.Concat(Convert.ToInt32(100 * e.BytesTransferred / e.TotalBytesToTransfer) + " %");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.facebook.com/wesley.vale.3192");
+            MessageBox.Show("Aplicação desenvolvida para uso publico, todos os creditos ao desenvolvedor.", "Wesley Vale", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
